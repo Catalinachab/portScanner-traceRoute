f5f0e3b1ee234ccc0f7a81d33b15af62105f03a0
Catalina Chab López <catalinachablopez@Catalinas-MacBook-Air-6.local>
2024-04-09 19:18:18 -0300

    Versión final

---
+++ portScanner.py
@@ -0,0 +1,109 @@
+from scapy.all import *
+import sys
+#import matplotlib.pyplot as plt
+
+
+
+results = "portScanner_"
+
+# funcion para enviar un paquete con payload
+def sendPayload(ipDst, port, ackRes):
+    # agrego el payload al paquete
+    packet = IP(dst= ipDst)/TCP(flags="A", sport = 8080 , dport = port, ack=ackRes+1 )/Raw(load="holaMundo")
+    resp = sr1(packet, timeout = 0.1)
+    # chequeo si me llego un ack del payload
+    if resp:
+        if resp[TCP].flags == "A":
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+# funcion para escanear un puerto
+def scanPort(ipDst, port, version):
+    # envio un paquete con el flag SYN
+    packet = IP(dst= ipDst)/TCP(flags="S", sport = 8080,seq = 0 , dport = port)
+    resp = sr1(packet, timeout = 0.1)
+    if resp:
+        # si recibo un paquete con el flag SYN/ACK
+        if resp[TCP].flags == "SA":
+            # si la version es -f envio un paquete con payload para chequear si esta abierto el puerto
+            if version == "-f":
+                ## envio un segundo mensaje con payload y chequeo si me lo ackea
+                ackRes = resp[TCP].seq
+                if sendPayload(ipDst, port, ackRes):
+                    return (port, "(abierto)")
+                else:
+                    return (port, "(cerrado)")  
+            # si la version es -h no envio el payload y lo tomo como abierto
+            elif version == "-h":
+                return (port, "(abierto)")
+        # si recibo un paquete con el flag RST/ACK lo tomo como cerrado
+        elif resp[TCP].flags == "RA":
+            return (port, "(cerrado)")
+        # caso en el que recibo un paquete con otro flag que no tome en cuenta
+        else:
+            return (port, "(raro)")
+    # si no recibo nada lo tomo como filtrado
+    else: 
+        return (port, "(filtrado)")
+
+# funcion para escanear los primeros 1000 puertos de una ip
+def portScanner(ipDst, version):
+    filtrados = 0
+    abiertos = 0
+    port = 1
+    # Define la ruta de la carpeta en la que deseas guardar el archivo
+    carpeta = "puertos_obs" + version
+    # Nombre del archivo
+    nombre_archivo = str(ipDst) + ".csv"
+    # Une la carpeta y el nombre del archivo para obtener la ruta completa
+    ruta_completa = os.path.join(carpeta, nombre_archivo)
+    # abro el archivo para escribir los resultados de todos los puertos
+    with open(ruta_completa, 'w') as file:
+        # columnas del csv
+        file.write("puerto,estado\n")
+        while port <= 1000:
+            portResult = scanPort(ipDst, port, version)
+            # appendeo el resultado al csv como puerto,estado
+            file.write(str(portResult[0]) + "," + portResult[1] + "\n")
+            # cuento los puertos abiertos y filtrados
+            if portResult[1] == "(abierto)":
+                abiertos += 1
+            elif portResult[1] == "(filtrado)":
+                filtrados += 1
+            port += 1
+        # imprimo los porcentajes de puertos abiertos y filtrados
+        print("porcentaje puertos abiertos: " + str((abiertos/1000)*100) + " %")
+        print("porcentaje puertos filtrados: " + str((filtrados/1000)*100) + " %")
+    
+    # abro el archivo para escribir los porcentajes de puertos abiertos y filtrados
+    with open("porcentajesPuertos.csv", 'a') as file:
+        # aca recorto el nombre del archivo
+        # asi solo se appendea la url
+        file.write(ipDst + "," + version +","+ str((abiertos/1000)*100) +","+ str((filtrados/1000)*100)+ "," + str( (1000 - abiertos - filtrados)/1000 * 100) + "\n")
+
+        ### DESCOMENTAR PARA GENERAR EL GRAFICO
+        ## NO SUELE DAR MUCHA INFORMACION
+        #plt.pie([abiertos/1000, filtrados/1000, (1000 - abiertos - filtrados)/1000],explode=(0.1, 0.1, 0.1), startangle=180, autopct='%1.0f%%',labeldistance=3.0, colors=[ '#87CEEB','#FFC0CB', '#98FB98'])
+        #plt.title("Porcentaje de puertos abiertos, filtrados y cerrados")
+        #plt.legend(["abiertos", "filtrados","cerrados"], title='Estados', loc='lower center', bbox_to_anchor=(1, 0.5))
+        #plt.axis('equal')
+        #plt.savefig(results + str(ipDst) + ".png")
+
+
+
+# MAIN
+# Verifico que se haya ingresado la direccion ip de destino correctamente
+if len(sys.argv) != 3:
+    print("Uso: python portScanner.py <direccionIpDst> <-version (-h / -f)>")
+    sys.exit(1)
+
+# Guardo la direccion ip de destino
+direccionTest = sys.argv[1]
+
+portScanner(direccionTest, sys.argv[2])
+
+# 80 y 443 son los puertos mas comunes abiertos ya que son los puertos de http y https respectivamente
---
+++ traceRoute.py
@@ -0,0 +1,101 @@
+from scapy.all import *
+import sys
+import time
+
+
+# calculador de promedios}
+## crea un csv con los resultados del traceRoute en la carpeta traceroutes_obs
+## filename: nombre del archivo a calcular
+## tambien calcula el promedio de los tiempos de RTT y lo appendea en el archivo porcentajesRouters.csv
+def calculate_mean(filename):
+    mean = 0
+    meanTiempos = 0
+    with open("traceroutes_obs/" + filename, 'r') as f:
+        lines = f.readlines()
+        # saco el header
+        lines.pop(0)
+        for line in lines:
+            line = line.split(",")
+            meanTiempos += float(line[2])
+            # incrementamos 1 por cada router que si responde
+            if line[1] != "*":
+                mean += 1    
+        # calculamos el promedio de routers que nos contestaron en el camino
+        mean = (mean/len(lines))*100 
+        # calculamos el promedio de los tiempos de RTT de todos los hops
+        meanTiempos = meanTiempos/len(lines)           
+    
+    with open("porcentajesRouters.csv", 'a') as file:
+        # aca recorto el nombre del archivo
+        # asi solo se apendea la url
+        file.write(filename[10:len(filename)-4] + "," +str(mean)+ "," + str(meanTiempos) + "\n")
+        print("promedio mensajes TTL0 recibidos: " + str(mean))
+        print("promedio tiempos de RTT de todos los hops: " + str(meanTiempos))
+
+
+
+
+
+
+## FUNCIONES ## definimos el traceRoute
+def traceRoute(direccionIpDst):
+    results = "traceRoute" + direccionIpDst + ".csv"
+    timeSnd = 0
+    timeRcv = 0
+    ttl = 1
+    # el flag indica cuando llegamos a destino
+    flag = True
+    
+    # obtengo la direccion ip de destino
+    direccionIpDst = socket.gethostbyname(direccionIpDst)
+
+
+    with open("traceroutes_obs/" + results, 'w') as file:
+        # columnas del csv
+        file.write("ttl,ip,RTT\n")
+        while flag:
+            packet = IP(dst= direccionIpDst, ttl = ttl)/ICMP(type=8, code=0)
+            # time agregado para calcular el tiempo de RTT
+            timeSnd = time.time()
+            resp = sr1(packet, timeout = 0.5)
+            timeRcv = time.time()
+        
+            ip = "*"
+            # chequeo si tengo un ip para escribir en el csv
+            if (resp is not None):
+                ip = str(resp.src)
+            # escribo en el csv el ttl, ip y el tiempo de RTT
+            file.write(str(ttl) + "," + ip + "," + str(timeRcv-timeSnd) + "\n")
+
+            if resp is None:
+                print("***")
+                ttl += 1
+            elif resp.src == direccionIpDst:
+                print("destino alcanzado en " + str(ttl) + " saltos y con RTT de: " + str(timeRcv - timeSnd) + " segundos")
+                flag = False
+            else:
+                #print(ttl)
+                print(resp.src)
+                ttl += 1
+    calculate_mean(results)
+
+
+
+
+## MAIN ##
+# Verifico que se haya ingresado la direccion ip de destino correctamente
+if len(sys.argv) != 2:
+    print("Uso: python traceRoute.py <direccionIpDst>")
+    sys.exit(1)
+
+# Guardo la direccion ip de destino
+direccionTest = sys.argv[1]
+
+# ejecuto el traceRoute 1 vez
+traceRoute(direccionTest)
+
+# ejecuto el traceRoute 30 veces
+#i = 0
+#while i < 30:
+#    traceRoute(direccionTest)
+#    i += 1
